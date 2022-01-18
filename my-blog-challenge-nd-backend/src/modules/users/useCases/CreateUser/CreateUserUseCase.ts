4129f23edee0e502b86f8d25c7b51d094502a0fb
Thaís de Freitas <thais.sdfreitas@gmail.com>
2022-01-18 15:17:25 -0300

    Create User

--- my-blog-challenge-nd-backend/src/modules/users/useCases/CreateUser/CreateUserUseCase.ts
+++ my-blog-challenge-nd-backend/src/modules/users/useCases/CreateUser/CreateUserUseCase.ts
@@ -11,8 +11,22 @@ export class CreateUserUseCase {
 	) { }
 
 	async execute({ email, password, name }: ICreateUserDTO): Promise<IUser> {
-		// TODO: Criar usuário, atente ao fato que dois usuários não podem possuir o mesmo email
+		// TODO: Criar usuário, atente ao fato que dois usuários não podem possuir o mesmo email - ok
 		// TODO: HashProvider pode ser utilizado para criptografia da senha
-		throw new AppError('Method not implemented!', 500);
-	}
+		const user = await this.usersRepository.findByEmail(email);
+
+		if (user) {
+			throw new AppError('Email já Cadastrado', 403);
+		}
+
+		const pass = await this.hashProvider.hash(password);
+		const dataSend = {email: email, password: pass, name: name };
+		const returnCreate = await this.usersRepository.create(dataSend);
+
+		if(!returnCreate){
+			throw new AppError('Erro ao Cadastrar Usuário', 403);
+		}
+
+		return returnCreate
+	}	
 }
